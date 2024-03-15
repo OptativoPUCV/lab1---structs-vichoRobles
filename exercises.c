a4a9909ca35af63bf4fb0f7805f22b40ca88622f
Your Name <you@example.com>
2024-03-15 00:18:01 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -156,7 +156,16 @@ typedef struct {
 } Libro;
 
 void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
-                      int anioNacimiento, int anioPublicacion) {}
+                      int anioNacimiento, int anioPublicacion) {
+  strcpy(libro->titulo, titulo);
+  strcpy(libro->autor.nombre, nombreAutor);
+  libro->autor.anioNacimiento = anioNacimiento;
+  libro->anioPublicacion = anioPublicacion;
+  
+  
+  
+  
+                      }
 
 /*
 Ejercicio 7: Lista enlazada de números
